64c2eb1c92a3339a600cdc1a8c0b8daa081e8798
Sergei Blinov <blinovsv@gmail.com>
2020-05-19 13:14:40 +0300

    update

--- kattis/sequences.py
+++ kattis/sequences.py
@@ -4,42 +4,20 @@ r = 0
 o = 0
 q = 500000004
 
-# s = open(0, 'rb').read()
-
-# for c in s:
-#     if c == 63:
-#         r <<= 1
-#         r += o * q
-#         r %= mod
-
-#         q <<= 1
-#         q %= mod
-
-#         o += 1
-#     elif c == 48:
-#         r += o * q
-#         r %= mod
-#     else:
-#         o += 2
-
-# f1 = lambda x: (x << 1) % p
-# f2 = lambda x: (x + o * q) % p
-
 f1 = lambda x, p: (x << 1) % p
 f2 = lambda x, o, q, p: (x + o * q) % p
 
 for c in open(0, 'rb').read():
+    if c == 63:
+        r = f1(r, mod)
+        r = f2(r, o, q, mod)
 
-        if c == 63:
-            r = f1(r, mod)
-            r = f2(r, o, q, mod)
+        q = f1(q, mod)
 
-            q = f1(q, mod)
-
-            o += 1
-        elif c == 48:
-            r = f2(r, o, q, mod)
-        else:
-            o += 2
+        o += 1
+    elif c == 48:
+        r = f2(r, o, q, mod)
+    else:
+        o += 2
 
 print(r)
