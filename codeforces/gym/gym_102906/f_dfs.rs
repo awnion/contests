4d2b305d32437b0bab6af821a7aa238fdbaf4474
Sergei Blinov <blinovsv@gmail.com>
2021-02-25 12:58:55 +0300

    update

--- codeforces/gym/gym_102906/f_dfs.rs
+++ codeforces/gym/gym_102906/f_dfs.rs
@@ -2,13 +2,15 @@
 #![allow(unused_imports, unused_macros)]
 
 use std::{
+    any::type_name,
     cmp,
     collections::*,
+    collections::HashMap,
     fmt::Debug,
     io::{self, prelude::*},
     iter,
     mem::{replace, swap},
-    str::FromStr,
+    str::FromStr
 };
 
 pub fn readline() -> String {
@@ -27,22 +29,54 @@ fn read_vi<T: FromStr>(_len: usize) -> Vec<T> {
     }).collect()
 }
 
-fn
-prep_refs(k: usize, n: usize, seq: &Vec<usize>)
--> Vec<Vec<usize>> {
-    let mut r = vec![vec![n + 1; n + 2]; k];
+fn prep_refs(k: usize, n: usize, seq: &Vec<usize>) -> Vec<usize> {
+    let mut r = vec![n + 1; 5005 * k];
     for i in 0..k {
         let mut cur = n + 1;
         for j in (0..n).rev() {
             if seq[j] == i + 1 {
                 cur = j + 1;
             }
-            r[i][j] = cur;
+            r[i * 5005 + j] = cur;
         }
     }
     r
 }
 
+struct FEnv {
+    na: Vec<usize>,
+    nb: Vec<usize>,
+    n: usize,
+    m: usize,
+    k: usize,
+}
+
+fn dp(e: &FEnv, i: usize, j: usize, cache: &mut[usize]) -> usize {
+    if i > e.n && j > e.m { return 0 }
+
+    let r = cache[i * 5005 + j];
+
+    if r < usize::MAX { return r }
+
+    let mut r = usize::MAX;
+    for el in 0..e.k {
+        r = r.min(dp(e, e.na[el * 5005 + i], e.nb[el * 5005 + j], cache) + 1)
+    }
+    cache[i * 5005 + j] = r;
+    r
+}
+
+
+fn backtrack(e: &FEnv, i: usize, j: usize, cache: &mut[usize], r: usize) {
+    if r == 0 { return }
+    for el in 0..e.k {
+        if r - 1 == dp(e, e.na[el * 5005 + i], e.nb[el * 5005 + j], cache) {
+            print!("{} ", el + 1);
+            backtrack(e, e.na[el * 5005 + i], e.nb[el * 5005 + j], cache, r - 1);
+            break;
+        }
+    }
+}
 
 fn main() {
     let k: usize = read_i();
@@ -56,54 +90,15 @@ fn main() {
 
     if k == 1 {
         println!("{}", n.max(m) + 1);
-        for _ in 0..n.max(m) + 1 {
-            print!("1 ");
-        }
+        for _ in 0..=n.max(m) { print!("1 ") }
         return;
     }
 
     let na = prep_refs(k, n, &a);
     let nb = prep_refs(k, m, &b);
 
-    let mut cache = vec![vec![usize::MAX; m + 2]; n + 2];
-
-    struct FEnv {
-        na: Vec<Vec<usize>>,
-        nb: Vec<Vec<usize>>,
-        n: usize,
-        m: usize,
-        k: usize,
-    };
-
-    let env = FEnv {
-        na: na,
-        nb: nb,
-        n: n,
-        m: m,
-        k: k,
-    };
-
-    fn dp(e: &FEnv, i: usize, j: usize, cache: &mut Vec<Vec<usize>>) -> usize {
-        if i > e.n && j > e.m { return 0 }
-        if cache[i][j] < usize::MAX { return cache[i][j] }
-        let mut r = usize::MAX;
-        for el in 0..e.k {
-            r = r.min(dp(e, e.na[el][i], e.nb[el][j], cache) + 1);
-        }
-        cache[i][j] = r;
-        r
-    };
-
-    fn backtrack(e: &FEnv, i: usize, j: usize, cache: &mut Vec<Vec<usize>>, r: usize) {
-        if r == 0 { return }
-        for el in 0..e.k {
-            if r - 1 == dp(e, e.na[el][i], e.nb[el][j], cache) {
-                print!("{} ", el + 1);
-                backtrack(e, e.na[el][i], e.nb[el][j], cache, r - 1);
-                break;
-            }
-        }
-    }
+    let mut cache = vec![usize::MAX; 5005 * 5005];
+    let env = FEnv { na, nb, n, m, k };
 
     let r = dp(&env, 0, 0, &mut cache);
     println!("{}", r);
